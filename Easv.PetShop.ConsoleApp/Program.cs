b3ec7ee3f056b681c54bdabd1433eab94131c5f3
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-08-29 10:25:29 +0200

    Add project files.

---
+++ Easv.PetShop.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Easv.PetShop.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
