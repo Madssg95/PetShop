1fdfcab33bb5d6531a8e0284df906bf679330d4b
Unknown <mads.gadeberg1995@gmail.com>
2018-08-30 18:23:23 +0200

    Commit of PetShop

---
+++ Easv.PetShop.Infrastructure.Data/FakeDB.cs
@@ -0,0 +1,13 @@
+﻿using Easv.PetShop.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easv.PetShop.Infrastructure.Data
+{
+    public class FakeDB
+    {
+        public static int PetId = 1;
+        public static IEnumerable<Pet> ListOfPets = new List<Pet>();
+    }
+}
