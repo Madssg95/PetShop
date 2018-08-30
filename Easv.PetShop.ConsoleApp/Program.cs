f8e8cdad096de9769ac483c79a2701511782e7b4
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-08-30 18:15:01 +0200

    Merged

--- Easv.PetShop.ConsoleApp/Program.cs
+++ Easv.PetShop.ConsoleApp/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Easv.PetShop.Core.Application_Service;
+using Easv.PetShop.Core.Application_Service.Service;
+using Easv.PetShop.Core.Domain_Service;
+using Easv.PetShop.Infrastructure.Data;
+using Easv.PetShop.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Easv.PetShop.ConsoleApp
 {
-    class Program
+    public class Program
     {
+        private static IPetRepository petRepository = new PetRepository();
+        private static IPetService petService = new PetService(petRepository);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<IPetRepository, PetRepository>() ;
+            serviceCollection.AddScoped<IPetService, PetService >();
+            serviceCollection.AddScoped<IPrinter, Printer>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var Printer = serviceProvider.GetRequiredService<IPrinter>();
+
+            Printer.StartUI();
+
+            Console.ReadLine();
         }
     }
 }
